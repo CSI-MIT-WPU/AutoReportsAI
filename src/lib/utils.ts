53b3db0336adf37c5c1e03a55d6a5f31d34c1b2b
Kandyyy <kingkandy016@gmail.com>
2024-09-21 12:59:53 +0530

    Added back convertFirestoreTimestampToDate() to lib/utils.ts to fix reports page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function convertFirestoreTimestampToDate(timestamp: {
+  seconds: number;
+  nanoseconds: number;
+}): Date {
+  try {
+    if (
+      timestamp &&
+      typeof timestamp.seconds === "number" &&
+      typeof timestamp.nanoseconds === "number"
+    ) {
+      return new Date(
+        timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+      );
+    }
+  } catch (error) {
+    console.error("Error formatting time:", error);
+  }
+  return new Date();
+}
